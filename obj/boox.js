5cc13d4ea7e4d06e1c3facb3989fd32302b84fdf
maysam <Maysssam@gmail.com>
2016-07-06 21:47:44 -0700

    add file

---
+++ obj/boox.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "boox.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 40,
+        "faces"         : 20,
+        "normals"       : 40,
+        "uvs"           : 40,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "buffers": "boox.bin"
+
+}
